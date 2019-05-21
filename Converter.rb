cff186cba456ce16e8a89262dd41f009ae155d6d
jjingo <sseguku@gmail.com>
2019-05-21 13:59:04 +0300

    Takes care of the correct allocation of and in the number in Thousand number

--- Converter.rb
+++ Converter.rb
@@ -12,84 +12,81 @@
 # 900       Nine Hundred
 # 1000100   One Million and Hundred
 # 3762      Three Thousand Seven Hundred Sixty Two
+class Words
+    def convertIntToWords (number)
+        #Hash representing concrete words
+        #representation
+        rep = {
+            1 => 'One',
+            2 => 'Two',
+            3 => 'Three',
+            4 => 'Four',        
+            5 => 'Five',
+            6 => 'Six',
+            7 => 'Seven',
+            8 => 'Eight',
+            9 => 'Nine',
+            10 => 'Ten',
+            11 => 'Eleven',
+            12 => 'Twelve',
+            13 => 'Thirteen',
+            14 => 'Fourteen',        
+            15 => 'Fifteen',
+            16 => 'Sixteen',
+            17 => 'Seventeen',
+            18 => 'Eighteen',
+            19 => 'Nineteen',
+            20 => 'Twenty',
+            30 => 'Thirty',
+            40 => 'Forty',
+            50 => 'Fifty',
+            60 => 'Sixty',
+            70 => 'Seventy',
+            80 => 'Eighty',
+            90 => 'Ninty',
+            #Realized this is not neccessary 
+            #100 => 'Hundred',
+            #1000 => 'Thousand',
+        # 1000000 => 'Million'
+        }
 
-def convertIntToWords (number)
-    #Hash representing concrete words
-    #representation
-    rep = {
-        1 => 'One',
-        2 => 'Two',
-        3 => 'Three',
-        4 => 'Four',        
-        5 => 'Five',
-        6 => 'Six',
-        7 => 'Seven',
-        8 => 'Eight',
-        9 => 'Nine',
-        10 => 'Ten',
-        11 => 'Eleven',
-        12 => 'Twelve',
-        13 => 'Thirteen',
-        14 => 'Fourteen',        
-        15 => 'Fifteen',
-        16 => 'Sixteen',
-        17 => 'Seventeen',
-        18 => 'Eighteen',
-        19 => 'Nineteen',
-        20 => 'Twenty',
-        30 => 'Thirty',
-        40 => 'Fourty',
-        50 => 'Fifty',
-        60 => 'Sixty',
-        70 => 'Seventy',
-        80 => 'Eighty',
-        90 => 'Ninty',
-        #Realized this is not neccessary 
-        #100 => 'Hundred',
-        #1000 => 'Thousand',
-       # 1000000 => 'Million'
-    }
 
+        #This is the output of integer in words
+        outputWords = ''
+        #numbers less than twenty
+        #Realized the solution has to be recursive
+        if number <= 20
+            outputWords = rep[number] 
+        #numbers less than hundred   
+        elsif number > 20 && number < 100         
+        number % 10 == 0 ? outputWords= rep[number] : outputWords = rep[(number / 10)* 10] + " "+ rep[(number % 10)] 
+        elsif number >= 100 && number < 1000
 
-    #This is the output of integer in words
-    outputWords = ''
-    #numbers less than twenty
-    #Realized the solution has to be recursive
-    if number <= 20
-        outputWords = rep[number] 
-     #numbers less than hundred   
-    elsif number > 20 && number < 100         
-     number % 10 == 0 ? outputWords= rep[number] : outputWords = rep[(number / 10)* 10] + " "+ rep[(number % 10)] 
-    elsif number >= 100 && number < 1000
-
-        #Caters for 100, 200, 300, 400, 500, 600, 700, 800, 900
-        if (number % 100 == 0)  
-            outputWords = rep[number/100] + ' Hundred '
-        else   
-        #Caters for all numbers whose reminder is not 0 when divided by 100
-            outputWords= rep[number / 100] + ' Hundred and ' + convertIntToWords(number % ((number /100)*100))
+            #Caters for 100, 200, 300, 400, 500, 600, 700, 800, 900
+            if (number % 100 == 0)  
+                outputWords = rep[number/100] + ' Hundred '
+            else   
+            #Caters for all numbers whose reminder is not 0 when divided by 100
+                outputWords= rep[number / 100] + ' Hundred and ' + convertIntToWords(number % ((number /100)*100))
+            end
+            #numbers less than a billion
+        elsif number >= 1000 && number < 1000000
+              number % 1000 == 0 ? 
+              outputWords = convertIntToWords( number/1000) + ' Thousand ' : 
+            
+            # # number % ((number /1000)*1000) < 1000  ?
+              (number % ((number /1000)*1000)) % 100 == 0 ? 
+              outputWords = convertIntToWords( number/1000) + ' Thousand and ' + convertIntToWords(number % ((number /1000)*1000)) :
+              outputWords = convertIntToWords( number/1000) + ' Thousand ' + convertIntToWords(number % ((number /1000)*1000))
+            # #numbers less than a 
+        elsif number >= 1000000 && number <1000000000
+            number % 1000000 == 0 ? 
+            outputWords = rep[number/1000000] + ' Million ' :
+            outputWords = rep[number / 1000000] + ' Million ' + convertIntToWords(number % ((number /1000000)*1000000))
         end
-        #numbers less than a billion
-    elsif number >= 1000 && number < 1000000
-        number % 1000 == 0 ? 
-         outputWords = rep[number/1000] + ' Thousand ' : 
-         
-         number % ((number /1000)*1000) < 100 ?
-         #caters for reminder of 1 upto 99
-         outputWords = rep[number / 1000] + ' Thousand and ' + convertIntToWords(number % ((number /1000)*1000)) :
-       
-         outputWords = rep[number / 1000] + ' Thousand ' + convertIntToWords(number % ((number /1000)*1000))
-         #numbers less than a 
-    elsif number >= 1000000 && number <1000000000
-        number % 1000000 == 0 ? 
-         outputWords = rep[number/1000000] + ' Million ' :
-         outputWords = rep[number / 1000000] + ' Million ' + convertIntToWords(number % ((number /1000000)*1000000))
-    end
-
-   #number % ((number /100)*100)
-   
-   outputWords
-end 
-
-
-puts convertIntToWords(1999)
+        # ((number /1000)*1000)
+        # outputWords
+    end 
+end
+@words = Words.new
+puts @words.convertIntToWords(543047)
--- specs.rb
+++ specs.rb
@@ -1,9 +1,45 @@
 require 'minitest/autorun'
 require_relative 'Converter'
 
-describe 'Numbers in words' do
-    it "should return Five"
-    
+describe Words do
+    before do
+        @words = Words.new
     end
+
+    describe "Should convert entered integers to words" do
+        it "must return Twelve" do
+           # @words.convertIntToWords(12).must_equal 'Twelves'
+           assert_equal "Twelve", @words.convertIntToWords(12)
+        end
+        it "must return Forty Two" do
+            # @words.convertIntToWords(12).must_equal 'Twelves'
+            assert_equal "Forty Two", @words.convertIntToWords(42)
+         end
+         
+        it "must return One Hundred and Twenty" do
+            # @words.convertIntToWords(120).must_equal 'Twelves'
+            assert_equal "One Hundred and Twenty", @words.convertIntToWords(120)
+         end
+         
+        it "must return Seven Hundred Eighty and Two" do
+            # @words.convertIntToWords(782).must_equal 'Twelves'
+            assert_equal "Seven Hundred and Eighty Two", @words.convertIntToWords(782)
+         end
+         it "must return Eight Thousand Three Hundred and Forty Four" do
+            # @words.convertIntToWords(8344).must_equal 'Twelves'
+            assert_equal "Eight Thousand Three Hundred and Forty Four", @words.convertIntToWords(8344)
+         end
+         it "must return Sixty Three Thousand Thousand Nine Hundred and Forty Seven" do
+            # @words.convertIntToWords(63947).must_equal 'Twelves'
+            assert_equal "Sixty Three Thousand Nine Hundred and Forty Seven", @words.convertIntToWords(63947)
+         end
+
+         
+         it "must return Five Hundred Forty Three Thousand and Forty Seven" do
+            # @words.convertIntToWords(543047).must_equal 'Twelves'
+            assert_equal "Five Hundred Forty Three Thousand and Forty Seven", @words.convertIntToWords(543047)
+         end
+    end
+
 
 end
